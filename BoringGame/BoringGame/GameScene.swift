e974c86d72809f2db636fbdad7e3d036053b5b7e
mengzhuou <a1106983163@gmail.com>
2024-02-18 18:52:06 -0500

    css of brick

--- BoringGame/BoringGame/GameScene.swift
+++ BoringGame/BoringGame/GameScene.swift
@@ -12,26 +12,39 @@ class GameScene: SKScene{
     
     override func didMove(to view: SKView) {
         
-        brick.size = CGSize(width: 50, height: 50)
+        brick.size = CGSize(width: 80, height: 50)
         brick.position = CGPoint(x: 100, y: 100)
-        brick.color = .blue
+        brick.color = .black
+        
+        
+        let brickText = SKLabelNode(text: "Boring")
+        brickText.color = .black
+        brickText.fontSize = 18
+        brickText.fontName = "Arial-BoldMT"
+        brickText.position = CGPoint(x: 0, y: 0)
+        brick.addChild(brickText)
         
         self.physicsWorld.gravity = CGVector(dx: 0.0, dy: -1.0)
         
         addChild(brick)
+        createGround()
     }
     
-//    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-//        for touch in touches {
-//            let location = touch.location(in: self)
-////            brick.position = location
-//            
-//            let action = SKAction.move(to: location, duration: 1.0)
-//            brick.run(action)
-//            
-//            
-//        }
-//    }
+    func createGround(){
+        let ground = SKSpriteNode()
+        ground.size = CGSize(width: 500, height: 5)
+        ground.color = .clear
+        ground.position = CGPoint(x: 250, y: 20)
+        
+        ground.physicsBody = SKPhysicsBody(rectangleOf: ground.size)
+        
+        ground.physicsBody?.isDynamic = false
+        ground.physicsBody?.affectedByGravity = false
+        
+        self.addChild(ground)
+        
+        
+    }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         for touch in touches {
