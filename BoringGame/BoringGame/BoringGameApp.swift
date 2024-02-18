0f40b7f9264946743f9ad482436c331ac704ad20
mengzhuou <a1106983163@gmail.com>
2024-02-18 16:42:02 -0500

    created template xcode

---
+++ BoringGame/BoringGame/BoringGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoringGameApp.swift
+//  BoringGame
+//
+//  Created by Mengzhu Ou on 2/17/24.
+//
+
+import SwiftUI
+
+@main
+struct BoringGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BoringGame/BoringGame/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  BoringGame
+//
+//  Created by Mengzhu Ou on 2/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var count = 0
+    var body: some View {
+        Text("Boring Game")
+    }
+}
+
+#Preview {
+    ContentView()
+}
